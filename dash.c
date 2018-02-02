2ab2360ab60b0d584c2ddbb1e66ac5321f732379
Lukas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-02-02 09:26:21 -0700

    Added a prompt for input loop, currently only takes in input and spits it back out unless the input is 'exit', next will be adding in handling for the commands and error messages for invalid input

--- dash.c
+++ dash.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+void handleInput(char* instr);
 
 int main(int argc, char*argv[]) {
-
-
+  char inbuffer[256] = "";
+  int running = 0;
+  while(running == 0) {
+    printf("dash> ");
+    scanf("%s", &inbuffer);
+    if(strcmp("exit",inbuffer) == 0) {
+      running++;
+    }
+    /** PRETTY UP THE STRING, SEND TO HANDLE FUNCTION **/
+  }
   return 0;
+}
+
+void handleInput(char* instr) {
+
 }
