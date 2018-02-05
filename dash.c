d53959b222ee6776bcd828069fcf8e6bd6734c40
Lukas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-02-05 09:30:18 -0700

    Added a startsWith function that compares two C strings and returns whether a given string starts with a given prefix; modified the command structure to include an int for the number of arguments passed in; added in checks for a given command through the handleCommand function; next will be adding in the proper help text and creating a function for each command.

--- dash.c
+++ dash.c
@@ -4,11 +4,13 @@
 
 struct op {
   char* name;
+  int argLength;
   char* args[10];
 };
 
 struct op handleInput(char* instr);
-
+void handleCommand(struct op command, int *running);
+int startsWith(const char *s1, const char*s2);
 
 int main(int argc, char*argv[]) {
   char inbuffer[256] = "";
@@ -17,12 +19,8 @@ int main(int argc, char*argv[]) {
   while(running == 0) {
     printf("dash> ");
     fgets(inbuffer, 250, stdin);
-    if(strcmp("exit",inbuffer) == 0) {
-      running++;
-    }
     command = handleInput(inbuffer);
-    if(strcmp(command.name, "exit"))
-      running++;
+    handleCommand(command, &running);
   }
   return 0;
 }
@@ -40,5 +38,37 @@ struct op handleInput(char* instr) {
     if(command.args[i] == NULL)
       break;
   }
+
+  command.argLength = i;
   return command;
+}
+
+void handleCommand(struct op command, int *running) {
+  if(startsWith("exit", command.name)) {
+    *running = *running + 1;
+  } else if(startsWith("cmdnm", command.name)) {
+    printf("Command name unfinished...\n");
+  } else if(startsWith("pid", command.name)) {
+    printf("PID unfinished...\n");
+  } else if(startsWith("systat", command.name)) {
+    printf("Systat unfinished...\n");
+  } else if(startsWith("help", command.name)) {
+    printf("There's no helping you...\n");
+  } else {
+    printf("Unknown command name. For a list of commands and their usage, type help.\n");
+  }
+}
+
+/** 
+ * int startsWith(const char*, const char*)
+ * - A helper function that compares if string 2 matches string 1
+ * - up to strlen(s1) characters. This is useful for when scanf picks
+ * - up the newline character and the same command becomes two different
+ * - commands in the eyes of strcmp.
+ * Arguments: const char* s1 - a prefix string
+ *            const char* s2 - a string to compare to s1
+ * Returns - 1 if s2 starts with s1, and 0 otherwise
+**/
+int startsWith(const char *s1, const char*s2) {
+  return strncmp(s1, s2, strlen(s1)) == 0;
 }
