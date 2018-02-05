71edddaebe0ab3edfa5c6e533907f17f95a191b9
Lukas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-02-04 22:44:39 -0700

    Added a struct for structuring commands; created a loop that reads in a line of input and dissects the tokens into a command name and arguments; added a handle that exits if the previously entered command name is 'exit'

--- dash.c
+++ dash.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void handleInput(char* instr);
+struct op {
+  char* name;
+  char* args[10];
+};
+
+struct op handleInput(char* instr);
+
 
 int main(int argc, char*argv[]) {
   char inbuffer[256] = "";
   int running = 0;
+  struct op command;
   while(running == 0) {
     printf("dash> ");
-    scanf("%s", &inbuffer);
+    fgets(inbuffer, 250, stdin);
     if(strcmp("exit",inbuffer) == 0) {
       running++;
     }
-    /** PRETTY UP THE STRING, SEND TO HANDLE FUNCTION **/
+    command = handleInput(inbuffer);
+    if(strcmp(command.name, "exit"))
+      running++;
   }
   return 0;
 }
 
-void handleInput(char* instr) {
-
+struct op handleInput(char* instr) {
+  char* tokens[10];
+  const char delim[2] = " ";
+  char* token;
+  struct op command;
+  token = strtok(instr, delim);
+  command.name = token;
+  int i = 0;
+  for(i = 0; i < 10; i++) {
+    command.args[i] = strtok(NULL, delim);
+    if(command.args[i] == NULL)
+      break;
+  }
+  return command;
 }
