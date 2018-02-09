f0ba6bf14b921ac897070c947deeac7536dff33c
Lukas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-02-09 11:19:29 -0700

    Added uptime output to systat command, if all works will be working on cpuinfo output next.

--- commands.c
+++ commands.c
@@ -2,24 +2,41 @@
 
 void systat() {
     printf("Systat unfinished.......\n");
-    File *file;
+    FILE *file;
     char buffer[1024];
     size_t bytes_read;
     char* match;
     float clock_speed;
 
+    /** Open, read, and save to a buffer /proc/version for version information **/
     file = fopen("/proc/version", "r");
     bytes_read = fread(buffer, 1, sizeof(buffer), file);
     fclose(file);
-
+    /** If file is empty or file exceeds buffer size, error out and exit.. **/
     if(bytes_read == 0 || bytes_read == sizeof(buffer)) {
-        //panic, explode, self destruct, leave no witnesses
         printf("/proc/version is too big or nonexistent.\n");
         return;
     }
-
+    /** Append a null terminator '\0' and output version info **/
     buffer[bytes_read] = '\0';
     printf("\nversion:\n%s\n", buffer);
+
+
+    /** Open, read, and save to a buffer /proc/uptime for uptime information **/
+    file = fopen("/proc/uptime", "r");
+    bytes_read = fread(buffer, 1, sizeof(buffer), file);
+    fclose(file);
+    /** If file is empty or file exceeds buffer size, error out and exit.. **/
+    if(bytes_read == 0 || bytes_read == sizeof(buffer)) {
+        printf("/proc/uptime is too big or nonexistent.\n");
+        return;
+    }
+    /** Append a null terminator '\0' and output version info **/
+    buffer[bytes_read] = '\0';
+    printf("\nuptime:\n%s\n", buffer);
+
+    
+
 /*
     match = strstr(buffer, "cpu MHz");
     if(match == NULL) {
