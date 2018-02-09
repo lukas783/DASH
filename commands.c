00f2ebc7df9a4ad576e0e836bf92021238548777
Lukas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-02-09 11:05:09 -0700

    Continuing working on systat, testing out outputting version file, if all good will move onto uptime and then cpu info

--- commands.c
+++ commands.c
@@ -8,18 +8,19 @@ void systat() {
     char* match;
     float clock_speed;
 
-    file = fopen("/proc/cpuinfo", "r");
+    file = fopen("/proc/version", "r");
     bytes_read = fread(buffer, 1, sizeof(buffer), file);
     fclose(file);
 
     if(bytes_read == 0 || bytes_read == sizeof(buffer)) {
         //panic, explode, self destruct, leave no witnesses
-        printf("/proc/cpuinfo is too big or nonexistent.\n");
+        printf("/proc/version is too big or nonexistent.\n");
         return;
     }
 
     buffer[bytes_read] = '\0';
-
+    printf("\nversion:\n%s\n", buffer);
+/*
     match = strstr(buffer, "cpu MHz");
     if(match == NULL) {
         printf("/proc/cpuinfo is missing string 'cpu Mhz'\n");
@@ -27,5 +28,5 @@ void systat() {
     }
     sscanf(match, "cpu MHz :  %f", &clock_speed);
     printf("Clock speed is... %f\n", clock_speed);
-
+*/
 }
