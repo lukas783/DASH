7de1c6f8bf7f8f43f865c143064006533ffe3c1a
Lukas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-02-09 11:40:23 -0700

    Added in meminfo and testing cpuinfo output.

--- commands.c
+++ commands.c
@@ -1,12 +1,10 @@
 #include "commands.h"
 
 void systat() {
-    printf("Systat unfinished.......\n");
+    //printf("Systat unfinished.......\n");
     FILE *file;
     char buffer[1024];
     size_t bytes_read;
-    char* match;
-    float clock_speed;
 
     /** Open, read, and save to a buffer /proc/version for version information **/
     file = fopen("/proc/version", "r");
@@ -33,9 +31,37 @@ void systat() {
     }
     /** Append a null terminator '\0' and output version info **/
     buffer[bytes_read] = '\0';
-    printf("\nuptime:\n%s\n", buffer);
+    printf("uptime:\n%s\n", buffer);
 
+    /** Open, read, and save to a buffer /proc/uptime for uptime information **/
+    file = fopen("/proc/meminfo", "r");
+    bytes_read = fread(buffer, 1, sizeof(buffer), file);
+    fclose(file);
+    /** If file is empty or file exceeds buffer size, error out and exit.. **/
+    if(bytes_read == 0 || bytes_read == sizeof(buffer)) {
+        printf("/proc/meminfo is too big or nonexistent.\n");
+        return;
+    }
+    /** Append a null terminator '\0' and output version info **/
+    buffer[bytes_read] = '\0';
+    printf("meminfo:\n%s\n", buffer);
     
+
+    char* match;
+    float clock_speed;
+    /** Open, read, and save to a buffer /proc/uptime for uptime information **/
+    file = fopen("/proc/cpuinfo", "r");
+    bytes_read = fread(buffer, 1, sizeof(buffer), file);
+    fclose(file);
+    /** If file is empty or file exceeds buffer size, error out and exit.. **/
+    if(bytes_read == 0 || bytes_read == sizeof(buffer)) {
+        printf("/proc/cpuinfo is too big or nonexistent.\n");
+        return;
+    }
+    match = strstr(buffer, "cache size");
+    /** Append a null terminator '\0' and output version info **/
+    buffer[bytes_read] = '\0';
+    printf("cpuinfo:\n%s\n", match);
 
 /*
     match = strstr(buffer, "cpu MHz");
