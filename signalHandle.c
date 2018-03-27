b22e581b3aa0d28435b08a049e23858be053e1b0
Lucas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-03-26 18:55:58 -0600

    Expanded the signal handle class to contain more than just a simple signal number spitter-outer, gave each caught signal a full explanation and call out of the signal caught.

--- signalHandle.c
+++ signalHandle.c
@@ -17,5 +17,63 @@
  **/
 void handle_signal(int signal) {
   sigset_t pending;
-  printf("Signal %d caught!\n", signal);
+  switch(signal) {
+    case 1:
+      printf("SIGHUP caught, hangup detected on controlling terminal or death of controlling process.\n");
+    break;
+    case 2:
+      printf("SIGINT caught, CTRL+C detected, an interrupt from keyboard.\n");
+      exit(5);
+    break;
+    case 3:
+      printf("SIGQUIT caught, Quit from keyboard.\n");
+      exit(5);
+    break;
+    case 4:
+      printf("SIGILL caught, illegal instruction.\n");
+    break;
+    case 6:
+      printf("SIGABRT caught, abort signal from abort function.\n");
+    break;
+    case 7:
+    case 10:
+      printf("SIGBUS/SIGPOLL caught, buss error  or a pollable event detected.\n");
+    break;
+    case 8:
+      printf("SIGFPE caught, floating point exception.\n");
+    break;
+    case 9:
+      printf("SIGKILL caught, kill signal, can't catch!.\n");
+    break;
+    case 11:
+      printf("SIGSEGV caught, invalid memory reference (segmentation fault).\n");
+      exit(5);
+    break;
+    case 12:
+    case 31:
+      printf("SIGSYS caught, bad system call.\n");
+    break;
+    case 13:
+      printf("SIGPIPE caught, broken pipe discovered, patch the leak!\n");
+    break;
+    case 14:
+      printf("SIGALRM caught, I bet you thought I'd make that annoying sound again..\n");
+    break;
+    case 15:
+      printf("SIGTERM caught, termination signal, its a polite kill signal, still not catchable.\n");
+    break;
+    case 16:
+    case 30:
+      printf("SIGUSR1 caught, you should ask whoever sent that signal what they were trying to accomplish.");
+    break;
+    case 17:
+    case 18:
+    case 20:
+      printf("SIGCHLD caught, a child process has been killed.\n");
+    break;
+    
+    default:
+      printf("Signal %d caught! \n", signal);
+      break;
+  }
 }
